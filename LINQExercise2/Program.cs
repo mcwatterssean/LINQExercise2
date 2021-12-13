0648ed489f5ef79e96dc1469ed0e7ef0067ccb3e
Sean McWatters <mcwatterssean@gmail.com>
2021-12-13 18:24:24 -0500

    init

---
+++ LINQExercise2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQExercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> videoGames = new List<string>
+            {"Dota", "Forza", "Assassin's Creed", "Mirror's Edge" };
+        
+            videoGames.Sort((a,b) => (a.Length.CompareTo(b.Length)));
+
+            foreach(var game in videoGames)
+            {
+                Console.WriteLine(game);
+            }
+
+        }
+    }
+}
